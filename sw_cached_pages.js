ecad87723e5daf8fc799152da65d4e92298229a1
Eugene Petrovsky <Dezzinlol2@gmail.com>
2019-04-06 19:41:17 +0300

    Initial commit

---
+++ sw_cached_pages.js
@@ -0,0 +1,42 @@
+const cacheName = 'v2';
+
+self.addEventListener('install', (event) => {
+  console.log('ServiceWorker: Installed');
+
+
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('ServiceWorker: Activated');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if( cache !== cacheName ) {
+            console.log('Service Worker: Clearing Old Cache');
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  )
+});
+
+self.addEventListener('fetch', e => {
+  console.log('Service Worker: Fetching');
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        //Make copy/clone of response
+        const resClone = res.clone();
+        //Open Cache
+        caches
+          .open(cacheName)
+          .then(cache => {
+            // Add response to cache
+            cache.put(e.request, resClone);
+          });
+          return res;  
+      }).catch(err => caches.match(e.request).then(res => res))
+  )
+});
