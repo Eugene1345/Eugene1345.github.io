4a4b056f643330d43c7e86438a24ce8b8afe52fd
Eugene Petrovsky <Dezzinlol2@gmail.com>
2019-04-10 10:16:54 +0300

    Test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/sw_cached_pages.js')
-      .then(reg => console.log('Register'))
-      .catch(err => console.log(err));
-  });
-} 
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/sw_cached_pages.js')
+//       .then(reg => console.log('Register'))
+//       .catch(err => console.log(err));
+//   });
+// } 
+
+if ('caches' in window) {
+  // The Cache API is supported
+  const cacheName = 'my-website-cache';
+  const cacheAssets = '/css/main.css';
+
+  caches.open(cacheName).then(cache => {
+    // you can start using the cache
+    console.log('yes');
+    cache.add(cacheAssets).then(() => {
+      console.log('added');
+    });
+
+    cache.match(cacheAssets).then( item => {
+      console.log(item);
+    })
+
+     fetch(cacheAssets).then(res => {
+       return caches.open(cacheName).then(cache => {
+         return cache.put(cacheAssets, res)
+      })
+     })
+    })
+  }
--- sw_cached_pages.js
+++ sw_cached_pages.js
@@ -1,42 +1,54 @@
-const cacheName = 'v2';
+// const cacheName = 'v2';
 
-self.addEventListener('install', (event) => {
-  console.log('ServiceWorker: Installed');
+// const cacheAssets = [
+//   '/css/main.css'
+// ]
 
+// self.addEventListener('install', (event) => {
+//   console.log('ServiceWorker: Installed');
 
-});
+//   event.waitUntil(
+//     caches
+//       .open(cacheName)
+//       .then(cache => {
+//         console.log('Service Worker: Caching Files');
+//         cache.addAll(cacheAssets);
+//       })
+//       .then(() => self.skipWaiting())
+//   )
+// });
 
-self.addEventListener('activate', (event) => {
-  console.log('ServiceWorker: Activated');
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if( cache !== cacheName ) {
-            console.log('Service Worker: Clearing Old Cache');
-            return caches.delete(cache);
-          }
-        })
-      )
-    })
-  )
-});
+// self.addEventListener('activate', (event) => {
+//   console.log('ServiceWorker: Activated');
+//   // event.waitUntil(
+//   //   caches.keys().then(cacheNames => {
+//   //     return Promise.all(
+//   //       cacheNames.map(cache => {
+//   //         if( cache !== cacheName ) {
+//   //           console.log('Service Worker: Clearing Old Cache');
+//   //           return caches.delete(cache);
+//   //         }
+//   //       })
+//   //     )
+//   //   })
+//   // )
+// });
 
-self.addEventListener('fetch', e => {
-  console.log('Service Worker: Fetching');
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        //Make copy/clone of response
-        const resClone = res.clone();
-        //Open Cache
-        caches
-          .open(cacheName)
-          .then(cache => {
-            // Add response to cache
-            cache.put(e.request, resClone);
-          });
-          return res;  
-      }).catch(err => caches.match(e.request).then(res => res))
-  )
-});
+// // self.addEventListener('fetch', e => {
+// //   console.log('Service Worker: Fetching');
+// //   e.respondWith(
+// //     fetch(e.request)
+// //       .then(res => {
+// //         //Make copy/clone of response
+// //         const resClone = res.clone();
+// //         //Open Cache
+// //         caches
+// //           .open(cacheName)
+// //           .then(cache => {
+// //             // Add response to cache
+// //             cache.put(e.request, resClone);
+// //           });
+// //           return res;  
+// //       }).catch(err => caches.match(e.request).then(res => res))
+// //   )
+// // });
